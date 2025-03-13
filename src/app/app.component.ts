60e5425c80589f4cad94a664660381da641b4fe0
GDevWeb <zodiaquestar@gmail.com>
2025-03-13 21:29:54 +0100

    ajout d'un filtre de recherche dynmaique avec utilisation de computed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, computed, inject, signal } from '@angular/core';
+import { PokemonBorderDirective } from './directives/pokemon-border.directive';
+import { Pokemon } from './pokemon.model';
+import { PokemonService } from './services/pokemon.service';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [PokemonBorderDirective, DatePipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-pokedex-app';
+  readonly #pokemonService = inject(PokemonService); //syntaxe moderne
+  readonly pokemonList = signal(this.#pokemonService.getPokemonList());
+  readonly searchTerm = signal('');
+
+  readonly pokemonListFiltered = computed(() => {
+    const searchTerm = this.searchTerm();
+    const pokemonList = this.pokemonList();
+
+    return pokemonList.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(searchTerm.trim().toLowerCase())
+    );
+  });
+
+  size(pokemon: Pokemon) {
+    if (pokemon.life <= 15) {
+      return 'Petit';
+    } else if (pokemon.life >= 25) {
+      return 'Grand';
+    } else {
+      return 'Moyen';
+    }
+  }
+
+  incrementLife(pokemon: Pokemon): number {
+    return (pokemon.life = pokemon.life + 1);
+  }
+
+  decrementLife(pokemon: Pokemon): number {
+    if (pokemon.life <= 0) {
+      return (pokemon.life = 0);
+    }
+    return (pokemon.life = pokemon.life - 1);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+registerLocaleData(localeFr, 'fr-FR');
